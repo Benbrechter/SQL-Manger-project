37d728b93c50e2e74ef295ece96d4cd9d8186b10
Benjamin Brechter <benbrechter3@gmail.com>
2024-03-21 14:28:51 -0500

    I added the correct Javascript

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,26 @@ USE department;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL;
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL;
-    salary INT NOT NULL;
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
     department_id INT,
-    FOREIGN KEY (department)
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees(
-    id INT NOT NULL AUTO_INCREMENT;
-    first_name VARCHAR(30) NOT NULL;
-    last_name VARCHAR(30) NOT NULL;
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    FOREIGN KEY (roles)
+    manager_id INT,
+    FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL
-
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES ("Engineering"),
        ("Legal"),
        ("Sales");
 
-INSERT INTO roles (title, department, salary)
+INSERT INTO roles (title, department_id, salary)
 VALUES ("Sales Lead", 4, 100000),
        ("Salesperson", 4, 80000),
        ("Lead Engineer", 1, 150000),
@@ -14,12 +14,12 @@ VALUES ("Sales Lead", 4, 100000),
        ("Legal Team Lead", 3, 250000),
        ("Lawyer", 3, 190000);
 
-INSERT INTO employees (first_name, last_name, role_id)
-VALUES ("Allen","Joe", 1),
-       ("Max", "Craft", 2),
-       ("Sam", "Mosley", 3),
-       ("Benjamin", "Brechter", 4),
-       ("Roe", "Joe", 5),
-       ("Erwin", "Collier", 6),
-       ("Paul", "Atrieties", 7),
-       ("Jessic", "Geserit", 8);
+INSERT INTO employees (first_name, last_name, role_id,manager_id)
+VALUES ("Allen","Joe", 1, NULL),
+       ("Max", "Craft", 2, 1),
+       ("Sam", "Mosley", 3, 2),
+       ("Benjamin", "Brechter", 4, 3),
+       ("Roe", "Joe", 5, 4),
+       ("Erwin", "Collier", 6, 5),
+       ("Paul", "Atrieties", 7, NULL),
+       ("Jessic", "Geserit", 8, 7);
